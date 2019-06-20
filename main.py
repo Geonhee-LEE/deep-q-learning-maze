4c0216f96bb3a7d41e3dee80592d71a3f708ad0e
Geonhee-LEE <gunhee6392@gmail.com>
2019-06-20 23:23:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,82 @@
 import argparse
+from collections import deque
+import numpy as np
 from DQNAgent import DQNAgent
 from GridWorld import GridWorld
 
-
+# Hyperparameter 값 받을 수 있도록 구성.
 PARSER = argparse.ArgumentParser(description=None)
 PARSER.add_argument('-lr', '--learning_rate', default=0.01, type=float, help='Learning rate')
-PARSER.add_argument('-gamma', '--discount_factor', default=1.0, type=float, help='Discounted factor')
-PARSER.add_argument('-e', '--epsilon', default=0.9, type=float, help='discount factor')
-PARSER.add_argument('-op', '--update_operation', default=100, type=int, help='Update operation of network')
-PARSER.add_argument('-ep', '--episode', default=1000, type=int, help='Training episode iteration number')
-PARSER.add_argument('-ms', '--memory_size', default=1000, type=int, help='Replay memory size')
+PARSER.add_argument('-discount_factor', '--discount_factor', default=1.0, type=float, help='Discounted factor')
+PARSER.add_argument('-e', '--epsilon', default=1.0, type=float, help='discount factor')
+PARSER.add_argument('-ep', '--episode', default=1500, type=int, help='Training episode iteration number')
+PARSER.add_argument('-ms', '--memory_size', default=5000, type=int, help='Replay memory size')
 PARSER.add_argument('-bs', '--batch_size', default=32, type=int, help='Batch size')
-PARSER.add_argument('-r', '--render', default=True, type=bool, help='Render')
-PARSER.add_argument('-st', '--start_epi', default=10, type=int, help='Render')
+PARSER.add_argument('-r', '--render', default=True, type=int, help='Render')
+PARSER.add_argument('-mes', '--max_episode_steps', default=100, type=bool, help='Maximum step num')
 ARGS = PARSER.parse_args()
 print (ARGS)
 
+avg_return_list = deque(maxlen=10)
+avg_loss_list = deque(maxlen=10)
+
 def main():
-    succ_cnt, fail_cnt = 0, 0
-    for episode in range(ARGS.episode):
-        state = env.reset()
-        while True:
+    for i in range(ARGS.episode):
+        obs = env.reset()        
+        done = ''
+        total_reward = 0
+        total_loss = 0
+        for _ in range(ARGS.max_episode_steps):
             # DQN모델에서 현재 state를 인자로 받아 action을 선택
-            action = agent.choose_action(state)
+            action = agent.get_action(obs)
 
             # Action을 취하여 환경에서 next state, reward, done 반환
-            next_state, reward, done = env.step(action)            
-            
-            # Reward가 0인 invalid한 action일때는 memory에 저장하지 않도록 구성.
-            if reward is not 0:
-                agent.add_memory(state, action, reward, next_state)
+            next_obs, reward, done = env.step(action)
 
-            # Next state를 state에 저장.
-            state = next_state
-            if done == 'DONE':
-                succ_cnt += 1
-                break
-            elif done == 'FAIL':
-                fail_cnt += 1
-                break
+            # Replay memory에 experience 추가.
+            if reward is not 0 :
+                agent.add_experience(obs,action,reward,next_obs,done)
             
-        # Replay memory stack 후 진행해야 하며, episode가 10번 실행 후에 agent 학습
-        if (episode >= ARGS.start_epi):
-            agent.train()
-            acc = succ_cnt/(succ_cnt + fail_cnt) * 100
-            print ('episode: ', episode, ', acc: ', acc , '%')      
+            # Prediction network 학습 및 policy 갱신(입실론값 변경)
+            loss = agent.train()
+            agent.update_policy()
+                    
+            obs = next_obs
+            total_reward += reward
+            total_loss += loss
+
+            if done == 'DONE' or done == 'FAIL':
+                break
+                
+        # Target network parameter 갱신
+        agent.update_target()
+        avg_return_list.append(total_reward)
+        avg_loss_list.append(total_loss)      
+        
+        # Logging
+        if (np.mean(avg_return_list) > 0.5): 
+            print('[{}/{}] loss : {:.3f}, return : {:.3f}, eps : {:.3f}'.format(i,ARGS.episode, np.mean(avg_loss_list), np.mean(avg_return_list), agent.epsilon))
+            print('The problem is solved with {} episodes'.format(i))
+            break        
+        if (i%100)==0:
+            print('[{}/{}] loss : {:.3f}, return : {:.3f}, eps : {:.3f}'.format(i,ARGS.episode, np.mean(avg_loss_list), np.mean(avg_return_list), agent.epsilon))                  
     env.grid_world.destroy()
 
 
 if __name__ == "__main__":
-    # GridWorld 
+    # GridWorld class instance 생성
     env = GridWorld(
              render = ARGS.render
              )
+    # DQN Agent class instance 생성
     agent = DQNAgent(
-             num_actions=env.num_actions, 
-             num_features=env.num_features,
-             update_operation=ARGS.update_operation,
+             n_action=env.n_action, 
+             obs_dim=env.obs_dim,
              memory_size=ARGS.memory_size,
              batch_size=ARGS.batch_size,
              lr=ARGS.learning_rate,
-             gamma=ARGS.discount_factor,
+             discount_factor=ARGS.discount_factor,
              epsilon=ARGS.epsilon
              )
-    # give delay for GridWorld 
     env.grid_world.after(100, main)
     env.grid_world.mainloop()
